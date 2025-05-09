7a5fff1a376df5d37d0d5b06a7bd8803f6691cc3
ateliersjp <h-edge@mail.ee>
2025-05-10 01:54:01 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,156 +1,88 @@
-// Website you intended to retrieve for users.
-const upstream = 'www.google.com'
+const instance_key = 'current_instance';
+const instance_pattern = /<td align="left"><a href="https:\/\/([^\/"]+)\/?" rel="nofollow">[^<]+<\/a><\/td>\n<td align="left">✅<\/td>\n<td align="left">✅<\/td>/g;
 
-// Custom pathname for the upstream website.
-const upstream_path = '/'
+export default {
+    async fetch(request, env) {
+        let url = new URL(request.url);
+        let url_hostname = url.hostname;
 
-// Website you intended to retrieve for users using mobile devices.
-const upstream_mobile = 'www.google.com'
+        url.host = await get_current_instance(env);
 
-// Countries and regions where you wish to suspend your service.
-const blocked_region = ['CN', 'KP', 'SY', 'PK', 'CU']
-
-// IP addresses which you wish to block from using your service.
-const blocked_ip_address = ['0.0.0.0', '127.0.0.1']
-
-// Whether to use HTTPS protocol for upstream address.
-const https = true
-
-// Whether to disable cache.
-const disable_cache = false
-
-// Replace texts.
-const replace_dict = {
-    '$upstream': '$custom_domain',
-    '//google.com': ''
-}
-
-addEventListener('fetch', event => {
-    event.respondWith(fetchAndApply(event.request));
-})
-
-async function fetchAndApply(request) {
-    const region = request.headers.get('cf-ipcountry').toUpperCase();
-    const ip_address = request.headers.get('cf-connecting-ip');
-    const user_agent = request.headers.get('user-agent');
-
-    let response = null;
-    let url = new URL(request.url);
-    let url_hostname = url.hostname;
-
-    if (https == true) {
-        url.protocol = 'https:';
-    } else {
-        url.protocol = 'http:';
-    }
-
-    if (await device_status(user_agent)) {
-        var upstream_domain = upstream;
-    } else {
-        var upstream_domain = upstream_mobile;
-    }
-
-    url.host = upstream_domain;
-    if (url.pathname == '/') {
-        url.pathname = upstream_path;
-    } else {
-        url.pathname = upstream_path + url.pathname;
-    }
-
-    if (blocked_region.includes(region)) {
-        response = new Response('Access denied: WorkersProxy is not available in your region yet.', {
-            status: 403
-        });
-    } else if (blocked_ip_address.includes(ip_address)) {
-        response = new Response('Access denied: Your IP address is blocked by WorkersProxy.', {
-            status: 403
-        });
-    } else {
         let method = request.method;
-        let request_headers = request.headers;
-        let new_request_headers = new Headers(request_headers);
+        let body = request.body;
+        let headers = replace_headers(request.headers, url_hostname, url.host);
 
-        new_request_headers.set('Host', upstream_domain);
-        new_request_headers.set('Referer', url.protocol + '//' + url_hostname);
+        headers.delete('Content-Length');
 
-        let original_response = await fetch(url.href, {
-            method: method,
-            headers: new_request_headers
-        })
-
-        connection_upgrade = new_request_headers.get("Upgrade");
-        if (connection_upgrade && connection_upgrade.toLowerCase() == "websocket") {
-            return original_response;
+        try {
+            let original_response = await fetch(url.href, { method, headers, body });
+            return handle_response(original_response, env);
+        } catch {
+            await update_current_instance(env);
+            return Response.redirect(url.href);
         }
-
-        let original_response_clone = original_response.clone();
-        let original_text = null;
-        let response_headers = original_response.headers;
-        let new_response_headers = new Headers(response_headers);
-        let status = original_response.status;
-		
-		if (disable_cache) {
-			new_response_headers.set('Cache-Control', 'no-store');
-	    }
-
-        new_response_headers.set('access-control-allow-origin', '*');
-        new_response_headers.set('access-control-allow-credentials', true);
-        new_response_headers.delete('content-security-policy');
-        new_response_headers.delete('content-security-policy-report-only');
-        new_response_headers.delete('clear-site-data');
-		
-		if (new_response_headers.get("x-pjax-url")) {
-            new_response_headers.set("x-pjax-url", response_headers.get("x-pjax-url").replace("//" + upstream_domain, "//" + url_hostname));
-        }
-		
-        const content_type = new_response_headers.get('content-type');
-        if (content_type != null && content_type.includes('text/html') && content_type.includes('UTF-8')) {
-            original_text = await replace_response_text(original_response_clone, upstream_domain, url_hostname);
-        } else {
-            original_text = original_response_clone.body
-        }
-		
-        response = new Response(original_text, {
-            status,
-            headers: new_response_headers
-        })
     }
-    return response;
 }
 
-async function replace_response_text(response, upstream_domain, host_name) {
-    let text = await response.text()
-
-    var i, j;
-    for (i in replace_dict) {
-        j = replace_dict[i]
-        if (i == '$upstream') {
-            i = upstream_domain
-        } else if (i == '$custom_domain') {
-            i = host_name
-        }
-
-        if (j == '$upstream') {
-            j = upstream_domain
-        } else if (j == '$custom_domain') {
-            j = host_name
-        }
-
-        let re = new RegExp(i, 'g')
-        text = text.replace(re, j);
+async function handle_response(original_response, env) {
+    let connection_upgrade = headers.get("Upgrade");
+    if (connection_upgrade && connection_upgrade.toLowerCase() == "websocket") {
+        return original_response;
     }
-    return text;
+
+    let status = original_response.status;
+    if (status != 200 && status != 404) {
+        await update_current_instance(env);
+        return Response.redirect(url.href);
+    }
+
+    let headers = replace_headers(original_response.headers, url.host, url_hostname);
+    let body = original_response.body;
+
+    headers.set('access-control-allow-origin', '*');
+    headers.set('access-control-allow-credentials', true);
+    headers.delete('content-security-policy');
+    headers.delete('content-security-policy-report-only');
+    headers.delete('clear-site-data');
+
+    return new Response(body, { status, headers });
 }
 
+function replace_headers(headers, before, after) {
+    let new_headers = new Headers();
+    let pattern = new RegExp('https?\\://' + before.replaceAll('.', '\\.'), 'g');
 
-async function device_status(user_agent_info) {
-    var agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
-    var flag = true;
-    for (var v = 0; v < agents.length; v++) {
-        if (user_agent_info.indexOf(agents[v]) > 0) {
-            flag = false;
-            break;
-        }
+    headers.forEach((value, key) => {
+        let new_value = value;
+        new_value = new_value.replace(pattern, 'https://' + after);
+        new_value = new_value.replaceAll(before, after);
+        new_headers.append(key, new_value);
+    });
+
+    return new_headers;
+}
+
+async function get_current_instance(env) {
+    let instance = await env.NITTER_KV_NAMESPACE.get(instance_key);
+
+    if (!instance) {
+        instance = await update_current_instance(env);
     }
-    return flag;
+
+    return instance;
+}
+
+async function update_current_instance(env) {
+    let response = await fetch('https://github.com/zedeus/nitter/wiki/Instances');
+    let response_text = await response.text();
+    let instance_value, instance = response_text.match(instance_pattern);
+
+    if (instance && instance[1]) {
+        instance_value = instance[1];
+        await env.NITTER_KV_NAMESPACE.put(instance_key, instance_value, {
+            expirationTtl: 3600,
+        });
+    }
+
+    return instance_value;
 }
